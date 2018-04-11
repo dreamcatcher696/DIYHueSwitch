5b0dced6abed4351552de36447c9b799b3093a44
dreamcatcher696 <thomaseckert@MacBook-Pro-van-Thomas.local>
2018-04-11 17:37:22 +0200

    added other buttons

--- DIYHueSwitch.ino
+++ DIYHueSwitch.ino
@@ -15,12 +15,16 @@ WiFiClient client;
 
 int butOnPin = 13;
 int butOffPin = 12;
+int butHighPin=14;
+int butLowPin=2;
 
 
 
 void setup() {
   pinMode(butOnPin,INPUT);
   pinMode(butOffPin,INPUT);
+  pinMode(butHighPin,INPUT);
+  pinMode(butLowPin,INPUT);
   Serial.begin(115200);
   delay(10);
 
@@ -45,7 +49,7 @@ void setHue(String command)
       if(client.connect(hueBridgeIP,hueHubPort))
       {
         client.print(String("PUT ") + "/api/32T4xilFgfRRBYtcXirBNbKMwvoGlYR3j6dtjWcj" + "/groups/1/action"\
-        + " HTTP/1.1\r\n" + "Host: " + host + "\r\n" + "Content-Length: " + command.length() + "\r\n"\
+        + " HTTP/1.1\r\n" + "Host: " + hueBridgeIP + "\r\n" + "Content-Length: " + command.length() + "\r\n"\
         + "Content-Type: text/plain;charset=UTF-8\r\n\r\n" + command + "\r\n" + "Connection: close\r\n\r\n");
       }
     
@@ -61,6 +65,38 @@ void loop() {
     while(digitalRead(butOnPin)==LOW && i < 30) {
       delay(20);
       i++;
+    }
+  }
+  if (digitalRead(butHighPin) == LOW) {
+    int i = 0;
+    while (digitalRead(butHighPin) == LOW && i < 30) {
+      delay(20);
+      i++;
+    }
+    if (i < 30) {
+      Serial.println("button high pressed(short)");
+      String command = "{\"on\": true, \"bri\":204,\"hue\":1876, \"sat\":254}";
+      setHue(command);
+    } else {
+      Serial.println("button high pressed(long)");
+      String command =  "{\"bri_inc\": 30}";
+      setHue(command);
+    }
+  }
+    if (digitalRead(butLowPin) == LOW) {
+    int i = 0;
+    while (digitalRead(butLowPin) == LOW && i < 30) {
+      delay(20);
+      i++;
+    }
+    if (i < 30) {
+      Serial.println("button low pressed(short)");
+      String command = "{\"on\": true, \"effect\":\"colorloop\"}";
+      setHue(command);
+    } else {
+      Serial.println("button low pressed(long)");
+      String command =  "{\"bri_inc\": -30}";
+      setHue(command);
     }
   }
   if(digitalRead(butOffPin)==LOW)
